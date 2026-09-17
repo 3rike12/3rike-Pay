4d86afd86718dd163766197e70dba5af1e60ebf2
Andrew David <andrewdvd10@gmail.com>
2026-09-17 11:57:13 +0100

    fix: format phone number with country code for createSubAccount
    - AutoRamp requires phone number to begin with country code
    - Convert user.phone from local 0803... to +234803... format

--- src/api/flow.ts
+++ src/api/flow.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { createLogger } from "@/utils/logger";
 import { autoramp } from "@/services/autoramp";
 import { prisma, updateSession, resetSession } from "@/services/database";
-import { generateReference } from "@/utils/helpers";
+import { generateReference, toWhatsAppPhone } from "@/utils/helpers";
 import { MESSAGES } from "@/config/constants";
 
 const logger = createLogger("flow");
@@ -156,7 +156,7 @@ async function handleOtp(userId: string, data: any) {
   try {
     logger.info("Creating AutoRamp sub-account", { userId, idType: flowData.idType });
     const subAccount = await autoramp.createSubAccount({
-      phoneNumber: user.phone,
+      phoneNumber: `+${toWhatsAppPhone(user.phone)}`,
       emailAddress: user.email || `${user.phone}@3rike.xyz`,
       externalReference: generateReference("kyc"),
       identityType: flowData.idType,
